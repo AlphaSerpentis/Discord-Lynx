8a8ae67910b55710342a0a727e04bb4d296352e5
AlphaSerpentis <37189157+AlphaSerpentis@users.noreply.github.com>
2019-06-20 12:47:42 -0500

    Pre-Release v0.01a (Build 2) for DJG
    Changed src structure

---
+++ src/main/commands/Kick.java
@@ -0,0 +1,35 @@
+package main.commands;
+
+import java.util.ArrayList;
+
+import main.init.InitData;
+import net.dv8tion.jda.core.entities.TextChannel;
+import net.dv8tion.jda.core.events.message.MessageReceivedEvent;
+
+public class Kick extends Command {
+
+	public Kick() {
+		super("kick", "Moderators can use this to kick users from the discord.\n\n`" + InitData.prefix + "kick @AlphaSerpentis#3203 \"Because he bad (please don't use that as an actual reason)\"`");
+		ArrayList<Long> ids = new ArrayList<Long>();
+		
+	}
+
+	@Override
+	public boolean action(TextChannel chn, String msg, Object misc) { 
+		
+		for(long id: getRoleIDs()) {
+			
+			if(id == ((MessageReceivedEvent) misc).getAuthor().getIdLong()) {
+				
+				String fullMsg, user, reason;
+				
+				return true;
+				
+			}
+			
+		}
+		
+		return false;
+	}
+
+}
